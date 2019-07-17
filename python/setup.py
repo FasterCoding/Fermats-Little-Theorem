1c150cc3e88660dbf53df5b11981491cea21367a
FasterCoding <fastercodingtutorial@gmail.com>
2019-07-17 16:18:14 +0200

    :recycle: Change Python Setup Version

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ with open("../README.md", "r") as fh:
 
 setuptools.setup(
     name="Fermat@FasterCoding",
-    version="1.0.0",
+    version="1.0.1",
     author="FasterCoding",
     author_email="fastercodingtutorial@gmail.com",
     description="Example code for the Fermats-Little-Theorem modular inverse algorithm",
